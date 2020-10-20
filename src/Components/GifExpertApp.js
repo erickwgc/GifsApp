79f7ac92f95d74daf1ed63f43595239817f17c2d
ErickGomez <erickxs78@gmail.com>
2020-10-20 18:04:43 -0400

    buscador de gifs funcionando

--- src/Components/GifExpertApp.js
+++ src/Components/GifExpertApp.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {AddCategory} from './AddCategory'
 import { GifGrid } from './GifGrid';
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState(['Dragon Ball']);
+    const [categories, setCategories] = useState(['Boruto']);
     // const handled = ()=>{
     //   setCategories(cats => [...cats,'Boruto']);
     // }
