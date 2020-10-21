da7aadff35f8f398d6c8006cb5c5e58727d24323
ErickGomez <erickxs78@gmail.com>
2020-10-21 09:08:53 -0400

    docs agregado

---
+++ docs/precache-manifest.a869ca591258f8cb81e7001a704a0420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cc70710768f446d8911763ae8469e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb1ab1e3846865c151ff",
+    "url": "/static/css/main.01f14b8f.chunk.css"
+  },
+  {
+    "revision": "94387244b6044698ff18",
+    "url": "/static/js/2.1d6add61.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1d6add61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1ab1e3846865c151ff",
+    "url": "/static/js/main.e7e480ba.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
